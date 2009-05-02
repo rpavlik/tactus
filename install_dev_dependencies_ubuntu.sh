d7d59985071e29f6e2e0f01ba07c2e7230c61547
Ryan Pavlik <ryan.pavlik@snc.edu>
2009-05-02 16:27:08 -0500

    Add script for dev dependencies.

---
+++ install_dev_dependencies_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+THISSCRIPT="install_dev_dependencies_ubuntu.sh"
+# Ryan Pavlik <ryan.pavlik@snc.edu> 2009
+
+# Ensures that dependencies are installed - these are only needed for development.
+
+echo "Installing dvelopment dependencies for tactus-navigator..."
+sudo aptitude install -y glade-gnome-3 drpython git-core giggle
+
+if [ "$(apt-cache search pymt | grep --invert-match None | grep -o Installed)" = "" ]; then
+	echo "You don't have the pymt package installed systemwide..."
+	echo "If you see 'No module named pymt' below, it's not installed at all"
+	echo "and you need to install it yourself one way or another!"
+	echo "I recommend the rp-mt-scripts to help you install it cleanly!"
+	echo "http://github.com/rpavlik/rp-mt-scripts/"
+	echo
+	python -c "import pymt; print pymt"
+else
+	echo "pymt package detected to be installed.  That's a good start."
+fi
+
